8e2dfdb16a3bc0b8bb5b44acd41bc23089a460ad
developedbyed <ed.magician@gmail.com>
2024-01-19 13:54:09 +0000

    auth

--- server/auth.ts
+++ server/auth.ts
@@ -3,6 +3,7 @@ import GitHub from "next-auth/providers/github"
 import { DrizzleAdapter } from "@auth/drizzle-adapter"
 import { db } from "@/server"
 import { eq } from "drizzle-orm"
+import { accounts, users } from "./schema"
 
 export const authConfig = {
   providers: [
@@ -14,10 +15,7 @@ export const authConfig = {
   ],
   adapter: DrizzleAdapter(db),
   callbacks: {
-    async session({ session, token }) {
-      if (session.user && token.sub) {
-        session.user.id = token.sub
-      }
+    async session({ session }) {
       return session
     },
     async jwt({ token }) {
